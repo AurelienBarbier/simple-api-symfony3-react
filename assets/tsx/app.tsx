c505d12baeb7248d63ba5251449faf8273222c7b
Aurelien Barbier <aurelien@wildcodeschool.fr>
2017-12-27 22:24:59 +0100

    Hello world works

---
+++ assets/tsx/app.tsx
@@ -0,0 +1,12 @@
+/**
+ * Created by lele on 27/12/17.
+ */
+
+import * as React from 'react'
+import {render} from 'react-dom'
+
+
+render(
+    <div> Salut les gens !!</div>,
+    document.getElementById('root')
+)
